94032d8ccc182b38216aa35ef19df5e92e86b4a7
JerryChen <pitaya943@icloud.com>
2022-03-24 20:17:03 +0800

    8二D

--- find_prime.py
+++ find_prime.py
@@ -1,7 +1,18 @@
+from ntpath import join
+from re import I
 from time import process_time
+
+bar_char = ['幹', '你', '娘'] * 20
+# dick = ['二'] * 20
+
+# A = 1
+# B = 1
+# C = 1
+# D = 1
 
 
 def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
+    # global A, B, C, D
     """
     Call in a loop to create terminal progress bar
     @params:
@@ -17,6 +28,24 @@ def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=
     percent = ("{0:." + str(decimals) + "f}").format(100 *
                                                      (iteration / float(total)))
     filledLength = int(length * iteration // total)
+
+    # if filledLength == 0 and A:
+    #     dick.insert(0, '8')
+    #     A = 0
+    # elif filledLength == 22 and B:
+    #     dick.append('D')
+    #     B = 0
+    # elif filledLength == 23 and C:
+    #     dick.append('-')
+    #     C = 0
+    # elif filledLength == 24 and D:
+    #     dick.append('3')
+    #     D = 0
+
+    # bar = ''.join(dick[0:int(filledLength/2)]) + \
+    #     '-' * (length - filledLength)
+    # bar = ''.join(bar_char[0:int(filledLength/2)]) + \
+    #     '-' * (length - filledLength)
     bar = fill * filledLength + '-' * (length - filledLength)
     print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
 
@@ -24,8 +53,8 @@ def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=
 targetNumber = int(
     input('Enter the number of defferent primes you want to find: '))
 
-specificIndex = int(
-    input('Enter the specific number of prime you want to find: '))
+# specificIndex = int(
+#     input('Enter the specific number of prime you want to find: '))
 
 primes = [] * targetNumber
 
